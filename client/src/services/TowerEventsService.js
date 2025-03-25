2854daae822d48f677ee17515f7a61bc2eefcdcb
d-hooper <dallinhoop3@gmail.com>
2025-03-25 16:22:31 -0600

    getting tower event json to render to page

--- client/src/services/TowerEventsService.js
+++ client/src/services/TowerEventsService.js
@@ -1,10 +1,13 @@
 import { logger } from "@/utils/Logger.js"
 import { api } from "./AxiosService.js"
+import { TowerEvent } from "@/models/TowerEvent.js"
+import { AppState } from "@/AppState.js"
 
 class TowerEventsService {
   async getAllTowerEvents() {
     const response = await api.get('api/events')
-    logger.log(response.data)
+    const towerEvents = response.data.map(towerEvent => new TowerEvent(towerEvent))
+    AppState.towerEvents = towerEvents
   }
 
 }
